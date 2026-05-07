af460a196b07a325fb38c49ea20c75d301799e3e
LenavarrDuoc <leo.navarr@duocuc.cl>
2026-05-07 16:48:38 -0400

    Initial commit

---
+++ src/test/java/cl/duoc/lmstockms/LmStockMsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.lmstockms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmStockMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
