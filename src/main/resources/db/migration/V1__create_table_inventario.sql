57d016d2b28e8fc8c1dda4c8f6a72b49cbcdc746
Josue-Solar <josu.solar@duocuc.cl>
2026-06-18 11:32:41 -0400

    lineas de definicion de perfiles test/dev

--- src/main/resources/db/migration/V1__create_table_inventario.sql
+++ src/main/resources/db/migration/V1__create_table_inventario.sql
@@ -8,22 +8,3 @@ CREATE TABLE IF NOT EXISTS stock_db (
                              CONSTRAINT uk_inventario_producto UNIQUE (fk_producto_id)
 );
 
-CREATE TABLE IF NOT EXISTS stock_db_dev (
-                                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                           fk_producto_id BIGINT NOT NULL,
-                                           cantidad INT NOT NULL,
-                                           estado BOOLEAN NOT NULL DEFAULT TRUE,
-
-    -- Restricción para asegurar que no se duplique el inventario de un mismo producto
-                                           CONSTRAINT uk_inventario_producto UNIQUE (fk_producto_id)
-    );
-
-CREATE TABLE IF NOT EXISTS stock_db_test (
-                                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                           fk_producto_id BIGINT NOT NULL,
-                                           cantidad INT NOT NULL,
-                                           estado BOOLEAN NOT NULL DEFAULT TRUE,
-
-    -- Restricción para asegurar que no se duplique el inventario de un mismo producto
-                                           CONSTRAINT uk_inventario_producto UNIQUE (fk_producto_id)
-    );
