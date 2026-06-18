f28509ea0aa99f75bd66f356e2669fa71487c854
Josue-Solar <josu.solar@duocuc.cl>
2026-06-17 21:18:48 -0400

    creados application.yml para entorno dev y testing

--- src/main/resources/db/migration/V1__create_table_inventario.sql
+++ src/main/resources/db/migration/V1__create_table_inventario.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS inventarios (
+CREATE TABLE IF NOT EXISTS stock_db (
                              id BIGINT AUTO_INCREMENT PRIMARY KEY,
                              fk_producto_id BIGINT NOT NULL,
                              cantidad INT NOT NULL,
@@ -7,3 +7,23 @@ CREATE TABLE IF NOT EXISTS inventarios (
     -- Restricción para asegurar que no se duplique el inventario de un mismo producto
                              CONSTRAINT uk_inventario_producto UNIQUE (fk_producto_id)
 );
+
+CREATE TABLE IF NOT EXISTS stock_db_dev (
+                                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                           fk_producto_id BIGINT NOT NULL,
+                                           cantidad INT NOT NULL,
+                                           estado BOOLEAN NOT NULL DEFAULT TRUE,
+
+    -- Restricción para asegurar que no se duplique el inventario de un mismo producto
+                                           CONSTRAINT uk_inventario_producto UNIQUE (fk_producto_id)
+    );
+
+CREATE TABLE IF NOT EXISTS stock_db_test (
+                                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                           fk_producto_id BIGINT NOT NULL,
+                                           cantidad INT NOT NULL,
+                                           estado BOOLEAN NOT NULL DEFAULT TRUE,
+
+    -- Restricción para asegurar que no se duplique el inventario de un mismo producto
+                                           CONSTRAINT uk_inventario_producto UNIQUE (fk_producto_id)
+    );
